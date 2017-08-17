a7d8a2d7c5055d975128d61dd5d2a5c571635c7f
Monique Hampton <moniquejhampton@yahoo.com>
2017-08-16 22:09:18 -0400

    html link update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendsData = require("../friends");
+var friendsData = require("../data/friends");
 
 
 
