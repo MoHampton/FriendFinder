e0dca93b87778385134548ac18d123cd2cec9c1f
Monique Hampton <moniquejhampton@yahoo.com>
2017-08-24 18:45:52 -0400

    still fixing

--- app/connection.js
+++ app/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'hacktheplanet',
+        password: 'monnyq',
         database: 'todoagain_db'
     }); 
 };
